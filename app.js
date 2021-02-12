b2e1a0295f9edb5ed237b1e93656deede89557a5
Kuldeep995 <60854358+Kuldeep995@users.noreply.github.com>
2021-02-12 22:46:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app= express();
+const request = require("request");
+const bodyParser = require("body-parser");
+const https=require("https");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+app.get("/", function(req, res){
+
+   res.sendFile(__dirname +"/index.html");
+
+})
+
+app.post("/", function(req, res){
+
+  console.log(req.body.fname);
+ const fn=req.body.fname;
+ const ln=req.body.lname;
+ const emai=req.body.email;
+
+ var data= {
+  members: [
+    {
+      email_address: emai,
+      status: "subscribed",
+      merge_fields:{
+        FNAME: fn,
+        LNAME: ln
+      }
+
+    }
+  ]
+}
+
+const jsonData = JSON.stringify(data);
+
+
+
+const url="https://us7.api.mailchimp.com/3.0/lists/8eb7f7eb73";
+
+const option= {
+
+method: "POST",
+
+auth: "" //(My API KEY)
+
+}
+const request=https.request(url, option, function(response){
+
+if(response.statusCode===200)
+{
+  res.sendFile(__dirname + "/success.html");
+
+}
+else
+{
+  res.sendFile(__dirname + "/failure.html");
+}
+response.on("data", function(data){
+
+  console.log(JSON.parse(data));
+})
+
+
+
+})
+
+request.write(jsonData);
+request.end();
+
+})
+
+app.post("/failure", function(req, res){
+
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("listening");
+
+})
+
+
